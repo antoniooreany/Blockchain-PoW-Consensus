fffaf16c47186f7273da7c903a35bfb1c4da1254
antoniooreany <antoniooreany@gmail.com>
2024-09-06 07:36:21 +0200

    Works with pow-blockchain-uml (2).mermaid

---
+++ block.py
@@ -0,0 +1,29 @@
+#   Copyright (c) 2024, Anton Gorshkov
+#   All rights reserved.
+#
+#   This code is for a block and its unit tests.
+#   For any questions or concerns, please contact Anton Gorshkov at antoniooreany@gmail.com
+
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash=''):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = 0  # Initialize nonce before calculating hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        sha = hashlib.sha256()
+        sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)).encode('utf-8'))
+        return sha.hexdigest()
+
+    def mine_block(self, difficulty):
+        target = '0' * difficulty
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        print(f"Block mined: {self.hash}")
--- blockchain.py
+++ blockchain.py
@@ -1,36 +1,84 @@
-#   Copyright (c) 2024, Anton Gorshkov
-#   All rights reserved.
+# #   Copyright (c) 2024, Anton Gorshkov
+# #   All rights reserved.
+# #
+# #   This code is for a blockchain.py and its unit tests.
+# #   For any questions or concerns, please contact Anton Gorshkov at antoniooreany@gmail.com
 #
-#   This code is for a blockchain.py and its unit tests.
-#   For any questions or concerns, please contact Anton Gorshkov at antoniooreany@gmail.com
-
-
-import hashlib
+#
+# import hashlib
+# import time
+#
+#
+# class Block:
+#     def __init__(self, index, timestamp, data, previous_hash=''):
+#         self.index = index
+#         self.timestamp = timestamp
+#         self.data = data
+#         self.previous_hash = previous_hash
+#         self.nonce = 0  # Initialize nonce before calculating hash
+#         self.hash = self.calculate_hash()
+#
+#     def calculate_hash(self):
+#         sha = hashlib.sha256()
+#         sha.update(
+#             (str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)).encode(
+#                 'utf-8'))
+#         return sha.hexdigest()
+#
+#     def mine_block(self, difficulty):
+#         target = '0' * difficulty
+#         while self.hash[:difficulty] != target:
+#             self.nonce += 1
+#             self.hash = self.calculate_hash()
+#         print(f"Block mined: {self.hash}")
+#
+#
+# class Blockchain:
+#     def __init__(self, difficulty):
+#         self.chain = [self.create_genesis_block()]
+#         self.difficulty = difficulty
+#
+#     def create_genesis_block(self):
+#         return Block(0, time.time(), "Genesis Block", "0")
+#
+#     def get_latest_block(self):
+#         return self.chain[-1]
+#
+#     def add_block(self, new_block):
+#         new_block.previous_hash = self.get_latest_block().hash
+#         new_block.mine_block(self.difficulty)
+#         self.chain.append(new_block)
+#
+#     def is_chain_valid(self):
+#         for i in range(1, len(self.chain)):
+#             current_block = self.chain[i]
+#             previous_block = self.chain[i - 1]
+#
+#             if current_block.hash != current_block.calculate_hash():
+#                 return False
+#
+#             if current_block.previous_hash != previous_block.hash:
+#                 return False
+#
+#         return True
+#
+#
+# class ProofOfWork:
+#     @staticmethod
+#     def find_nonce(block, difficulty):
+#         target = '0' * difficulty
+#         while block.hash[:difficulty] != target:
+#             block.nonce += 1
+#             block.hash = block.calculate_hash()
+#         return block.nonce
+#
+#     @staticmethod
+#     def validate_proof(block, difficulty):
+#         target = '0' * difficulty
+#         return block.hash[:difficulty] == target
 import time
 
-
-class Block:
-    def __init__(self, index, timestamp, data, previous_hash=''):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.nonce = 0  # Initialize nonce before calculating hash
-        self.hash = self.calculate_hash()
-
-    def calculate_hash(self):
-        sha = hashlib.sha256()
-        sha.update(
-            (str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)).encode(
-                'utf-8'))
-        return sha.hexdigest()
-
-    def mine_block(self, difficulty):
-        target = '0' * difficulty
-        while self.hash[:difficulty] != target:
-            self.nonce += 1
-            self.hash = self.calculate_hash()
-        print(f"Block mined: {self.hash}")
+from block import Block
 
 
 class Blockchain:
@@ -62,17 +110,3 @@ class Blockchain:
 
         return True
 
-
-class ProofOfWork:
-    @staticmethod
-    def find_nonce(block, difficulty):
-        target = '0' * difficulty
-        while block.hash[:difficulty] != target:
-            block.nonce += 1
-            block.hash = block.calculate_hash()
-        return block.nonce
-
-    @staticmethod
-    def validate_proof(block, difficulty):
-        target = '0' * difficulty
-        return block.hash[:difficulty] == target
