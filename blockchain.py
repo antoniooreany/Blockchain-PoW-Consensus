2d3fcafac4d3301c86a32881342444edfd136c40
antoniooreany <antoniooreany@gmail.com>
2024-09-06 17:44:05 +0200

    Split Block and Blockchain to the different files

--- blockchain.py
+++ blockchain.py
@@ -8,29 +8,7 @@
 import hashlib
 import time
 
-
-class Block:
-    def __init__(self, index, timestamp, data, previous_hash=''):
-        self.index = index
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
-        self.nonce = 0  # Initialize nonce before calculating hash
-        self.hash = self.calculate_hash()
-
-    def calculate_hash(self):
-        sha = hashlib.sha256()
-        sha.update(
-            (str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)).encode(
-                'utf-8'))
-        return sha.hexdigest()
-
-    def mine_block(self, difficulty):
-        target = '0' * difficulty
-        while self.hash[:difficulty] != target:
-            self.nonce += 1
-            self.hash = self.calculate_hash()
-        print(f"Block mined: {self.hash}")
+from block import Block
 
 
 class Blockchain:
