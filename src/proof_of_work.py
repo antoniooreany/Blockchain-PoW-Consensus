005ac91b56bd53443fdb10cfa3ee3bcf3944e5a8
antoniooreany <antoniooreany@gmail.com>
2024-10-13 19:03:37 +0200

    Add HASH_BIT_LENGTH to proof_of_work.py

--- src/proof_of_work.py
+++ src/proof_of_work.py
@@ -3,15 +3,18 @@
 #
 #   This code is for a pow and its unit tests.
 #   For any questions or concerns, please contact Anton Gorshkov at antoniooreany@gmail.com
-
+import math
 
 from block import Block
+
+HASH_BIT_LENGTH = 256  # The length of the hash in bits
+NONCE_BIT_LENGTH = 32  # The length of the nonce in bits
 
 
 # todo redundant class, move methods to Block class
 class ProofOfWork:
     @staticmethod
-    def find_nonce(block: Block, bit_difficulty: int) -> int:
+    def find_nonce(block: Block, bit_difficulty: float) -> int:  # todo implemented in Block
         """
         Finds a nonce for a given block such that the block's hash
         is less than the target value based on the difficulty.
@@ -23,14 +26,15 @@ class ProofOfWork:
         Returns:
             int: The nonce that results in the block's hash meeting the difficulty requirement.
         """
-        target_value = (2 ** (256 - bit_difficulty)) - 1
+        # target_value = (2 ** (256 - bit_difficulty)) - 1
+        target_value = math.pow(2, HASH_BIT_LENGTH - bit_difficulty) - 1
         while int(block.hash, 16) >= target_value:
             block.nonce += 1
             block.hash = block.calculate_hash()
         return block.nonce
 
     @staticmethod
-    def validate_proof(block: Block, bit_difficulty: int) -> bool:  # todo move to Block class, where to use it?
+    def validate_proof(block: Block, bit_difficulty: float) -> bool:  # todo move to Block class, where to use it?
         """
         Checks if a given block's hash is less than the target value based on the difficulty.
 
@@ -41,5 +45,5 @@ class ProofOfWork:
         Returns:
             bool: True if the block's hash meets the difficulty requirement, False otherwise.
         """
-        target_value = (2 ** (256 - bit_difficulty)) - 1
+        target_value = math.pow(2, HASH_BIT_LENGTH - bit_difficulty) - 1
         return int(block.hash, 16) < target_value
