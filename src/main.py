19fde3e769e0ac94154dba66b69831ca4324437d
antoniooreany <antoniooreany@gmail.com>
2024-10-14 13:54:15 +0200

    Comment ADJUSTMENT_INTERVAL, TARGET_BLOCK_TIME, CLAMP_FACTOR, SMALLEST_BIT_DIFFICULTY

--- src/main.py
+++ src/main.py
@@ -26,17 +26,17 @@ if __name__ == "__main__":
         # 16,
     ]:
         INITIAL_BIT_DIFFICULTY = 16  # todo avoid base_difficulty, use bit_difficulty, better even linear_difficulty
-        ADJUSTMENT_INTERVAL = 10
-        TARGET_BLOCK_TIME = 0.01
+        # ADJUSTMENT_INTERVAL = 10
+        # TARGET_BLOCK_TIME = 0.01
         NUMBER_BLOCKS_TO_ADD = 100
 
-        CLAMP_FACTOR = 2  # todo 2 bits; bin: 0b10, hex: 0x2, dec: 2: max adjustment factor
-        SMALLEST_BIT_DIFFICULTY = 4  # todo 4 bits; bin: 0b0000, hex: 0x0, dec: 0: smallest bit difficulty
+        # CLAMP_FACTOR = 2  # todo 2 bits; bin: 0b10, hex: 0x2, dec: 2: max adjustment factor
+        # SMALLEST_BIT_DIFFICULTY = 4  # todo 4 bits; bin: 0b0000, hex: 0x0, dec: 0: smallest bit difficulty
 
         blockchain = Blockchain(
             initial_bit_difficulty=INITIAL_BIT_DIFFICULTY,
-            adjustment_interval=ADJUSTMENT_INTERVAL,  # todo should it be a property of blockchain?
-            target_block_time=TARGET_BLOCK_TIME,
+            # adjustment_interval=ADJUSTMENT_INTERVAL,/  # todo should it be a property of blockchain?
+            # target_block_time=TARGET_BLOCK_TIME,
         )
         logger.debug(f"Created: blockchain (base: {base}, initial bit difficulty: {INITIAL_BIT_DIFFICULTY})")
         logger.debug(f"##################")
@@ -46,11 +46,17 @@ if __name__ == "__main__":
         log_mined_block(genesis_block)
         logger.debug(f"##################")
 
-        blockchain.mine_blocks(number_of_blocks=NUMBER_BLOCKS_TO_ADD, clamp_factor=CLAMP_FACTOR,
-                               smallest_bit_difficulty=SMALLEST_BIT_DIFFICULTY)
+        blockchain.mine_blocks(
+            number_of_blocks=NUMBER_BLOCKS_TO_ADD,
+            # clamp_factor=CLAMP_FACTOR,
+            # smallest_bit_difficulty=SMALLEST_BIT_DIFFICULTY,
+        )
 
         # Collect filtered bit difficulties
-        filtered_difficulties = collect_filtered_bit_difficulties(blockchain, ADJUSTMENT_INTERVAL)
+        filtered_difficulties = collect_filtered_bit_difficulties(
+            blockchain,
+            # ADJUSTMENT_INTERVAL,
+        )
         blockchain.bit_difficulties = filtered_difficulties  # Update the blockchain with filtered difficulties
 
         blockchains[base] = blockchain
