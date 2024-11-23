5ab11b237d35d9bb867ba242f6f78cb2e890dfbe
antoniooreany <antoniooreany@gmail.com>
2024-11-23 19:51:48 +0100

    Prepare hash_utils.py.

--- src/utils/hash_utils.py
+++ src/utils/hash_utils.py
@@ -13,11 +13,11 @@ def calculate_block_hash(
     """Compute the hash of a block by combining its attributes.
 
     Args:
-        index (int): The position of the block in the blockchain.
-        timestamp (float): The time at which the block was created.
-        data (str): The data contained within the block.
-        previous_block_hash (str): The hash of the previous block in the chain.
-        nonce (int): The nonce for the proof of work algorithm.
+        index: int: The position of the block in the blockchain.
+        timestamp: float: The time at which the block was created.
+        data: str: The data contained within the block.
+        previous_block_hash: str: The hash of the previous block in the chain.
+        nonce: int: The nonce for the proof of work algorithm.
 
     Returns:
         str: The hash of the block as a hexadecimal string.
@@ -26,12 +26,12 @@ def calculate_block_hash(
     validate_block_attributes(index=index, timestamp=timestamp, data=data, previous_block_hash=previous_block_hash, nonce=nonce)
 
     # Combine the arguments into a single string
-    data_to_hash = (
+    data_to_hash: bytes = (
         f"{index}{timestamp}{data}{previous_block_hash}{nonce}"
     ).encode(ENCODING)
 
     # Compute the hash of the string
-    hash_object = hashlib.sha256()
+    hash_object: hashlib._Hash = hashlib.sha256()
     hash_object.update(data_to_hash)
 
     # Return the hash as a hexadecimal string
@@ -43,7 +43,15 @@ def calculate_block_hash(
 def validate_block_attributes(
     index: int, timestamp: float, data: str, previous_block_hash: str, nonce: int
 ) -> None:
-    """Validate that all block attributes are not null or empty."""
+    """Validate that all block attributes are not null or empty.
+
+    Args:
+        index: int: The position of the block in the blockchain.
+        timestamp: float: The time at which the block was created.
+        data: str: The data contained within the block.
+        previous_block_hash: str: The hash of the previous block in the chain.
+        nonce: int: The nonce for the proof of work algorithm.
+    """
     for attribute, name in [
         (index, "index"),
         (timestamp, "timestamp"),
